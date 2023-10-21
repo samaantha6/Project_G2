dcb2d7577281377b1b5956fc3afaea8998f07f1b
sofia fernandez <so.fernandez@opendeusto.es>
2023-10-21 12:27:00 +0200

    mejoras interfaz ventana Registro

--- Project_G2/src/ventanas/VentanaRegistro.java
+++ Project_G2/src/ventanas/VentanaRegistro.java
@@ -16,7 +16,7 @@ public class VentanaRegistro extends JFrame{
 	
 	private JPanel pNorte, pSur, pOeste, pEste, pCentroIzq, pCentroDer, pCentro;
 	private JLabel txtReg, txtNombre, txtApellido, txtCorreo, txtTelefono, txtContrasenia, txtPregSeg, txtVenificaCon, txtRespuesta;
-	private JButton btnReg, btnVolver, btnOjo;
+	private JButton btnReg, btnVolver;
 	private JComboBox<String> pregSeg;
 	private JCheckBox Condiciones;
 	private JTextField campoReg, campoNombre, campoApellido, campoCorreo, campoTelefono, campoRespuesta;
@@ -25,27 +25,24 @@ public class VentanaRegistro extends JFrame{
 	public VentanaRegistro() {
 		
 		pNorte = new JPanel(new GridLayout(1,2));
-		pCentroIzq = new JPanel(new GridLayout(4,2));
-		pCentroDer = new JPanel(new GridLayout(4,2));
-		pCentro = new JPanel();
+		pCentro = new JPanel(new GridLayout(8,2));
 		pSur = new JPanel();
 		pOeste = new JPanel();
 		pEste = new JPanel();
 		
-		txtReg = new JLabel("REGISTRATE!!!!!!!!!");
-		txtNombre = new JLabel("Nombre");
-		txtApellido = new JLabel("Apellido");
-		txtCorreo = new JLabel("Correo");
-		txtTelefono = new JLabel("Telefono");
-		txtContrasenia = new JLabel("Contrasenia");
-		txtPregSeg = new JLabel("PregSeg");
-		txtVenificaCon = new JLabel("VenificaCon");
-		txtRespuesta = new JLabel("Respuesta");
+		txtReg = new JLabel("REGISTRATE:");
+		txtNombre = new JLabel("Nombre:");
+		txtApellido = new JLabel("Apellido:");
+		txtCorreo = new JLabel("Correo:");
+		txtTelefono = new JLabel("Telefono:");
+		txtContrasenia = new JLabel("Contrasenia:");
+		txtPregSeg = new JLabel("Pregunta de Seguridad:");
+		txtVenificaCon = new JLabel("Venifica la contraseña:");
+		txtRespuesta = new JLabel("Respuesta:");
 		
 
 		btnReg = new JButton("REGISTRARSE");
-		btnVolver = new JButton("volever");
-		btnOjo = new JButton("Ojo"); // aqui tenemos que ponder una imagen 
+		btnVolver = new JButton("volver");
 		
 		campoContrasenia = new JPasswordField();
 		campoVenificaCon = new JPasswordField();
@@ -57,32 +54,35 @@ public class VentanaRegistro extends JFrame{
 		campoTelefono = new JTextField();
 		campoRespuesta = new JTextField();
 
-		Condiciones = new JCheckBox("Acepto condiciones");
+		Condiciones = new JCheckBox("Acepto condiciones y términos de seguridad");
 		
 		pregSeg = new JComboBox<>();
 		
 		pNorte.add(txtReg);
+		
+		
 		//añadir imagen
-		pCentroIzq.add(txtNombre);
-		pCentroIzq.add(campoNombre);
-		pCentroIzq.add(txtCorreo);
-		pCentroIzq.add(campoCorreo);
-		pCentroIzq.add(campoContrasenia);
-		pCentroIzq.add(txtContrasenia);	//aqui iria el ojo
-		pCentroIzq.add(campoContrasenia);
-		pCentroIzq.add(txtPregSeg);
-		pCentroIzq.add(pregSeg);
+		pCentro.add(txtNombre);
+		pCentro.add(campoNombre);
+		pCentro.add(txtCorreo);
+		pCentro.add(campoCorreo);
+		pCentro.add(campoContrasenia);
+		pCentro.add(txtContrasenia);	//aqui iria el ojo
+		pCentro.add(campoContrasenia);
+		pCentro.add(txtPregSeg);
+		pCentro.add(pregSeg);
 
-		pCentroDer.add(txtApellido);
-		pCentroDer.add(campoApellido);
-		pCentroDer.add(txtTelefono);
-		pCentroDer.add(campoTelefono);
-		pCentroDer.add(txtVenificaCon);
-		pCentroDer.add(campoVenificaCon);
-		pCentroDer.add(txtRespuesta);
-		pCentroDer.add(campoRespuesta);
+		pCentro.add(txtApellido);
+		pCentro.add(campoApellido);
+		pCentro.add(txtTelefono);
+		pCentro.add(campoTelefono);
+		pCentro.add(txtVenificaCon);
+		pCentro.add(campoVenificaCon);
+		pCentro.add(txtRespuesta);
+		pCentro.add(campoRespuesta);
 
 		pSur.add(Condiciones);
+		
 		
 		pSur.add(btnReg);
 		pSur.add(btnVolver);	
@@ -92,8 +92,8 @@ public class VentanaRegistro extends JFrame{
 		this.add(pCentro,BorderLayout.CENTER);
 		this.add(pEste,BorderLayout.EAST);
 		this.add(pOeste,BorderLayout.WEST);
-		pCentro.add(pCentroIzq);
-		pCentro.add(pCentroDer);
+//		pCentro.add(pCentroIzq);
+//		pCentro.add(pCentroDer);
 		
 		this.setTitle("Registro");
 		this.setBounds(300, 200, 600, 400);
