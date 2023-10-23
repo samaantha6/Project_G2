72142353e3312408c88b067487378f7465854537
Samantha Muñoz <samantha.munoz@opendeusto.es>
2023-10-23 20:03:04 +0200

    Ventana Gestión de Empleados

--- Project_G2/src/ventanas/VentanaGestionEmpleados.java
+++ Project_G2/src/ventanas/VentanaGestionEmpleados.java
@@ -1,72 +1,109 @@
 package ventanas;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class VentanaGestionEmpleados extends JFrame {
+    private JPanel pNorte, pSur, pOeste, pEste, pCentro, pCentroDer, pCentroIzq, pCentroCen;
+    private JButton btnatras, btneliminar;
+    private JTextField nRef;
+    private JLabel txtEnv, txtRef, txtNull, imagenLabel;
+    private JTable table;
 
-public class VentanaGestionEmpleados {
+    public VentanaGestionEmpleados() {
+    	
+        pNorte = new JPanel(new GridLayout(1, 2));
+        pCentro = new JPanel(new GridLayout(6, 7)); 
+        pSur = new JPanel(new BorderLayout());
+        pCentroDer = new JPanel();
+        pCentroIzq = new JPanel();
+        pCentroCen = new JPanel();
+        pOeste = new JPanel(new GridLayout(5,5));
+        pEste = new JPanel();
+        
+    	imagenLabel = new JLabel();
 
+        txtEnv = new JLabel("Envíos realizados");
+        txtNull = new JLabel("");
+        txtRef = new JLabel("Nº Referencia");
 
-	    public static void main(String[] args) {
-	    	
-	        
-	        // Crear un marco (JFrame) para la aplicación
-	        JFrame frame = new JFrame("Ejemplo de Tabla en Java");
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        frame.setSize(400, 300);
+        btnatras = new JButton("<--");
+        btneliminar = new JButton("Eliminar Envío");
+        nRef = new JTextField(10);
+        
+    	pNorte.add(imagenLabel); //Imagen
 
-	        // Crear un modelo de tabla (DefaultTableModel)
-	        DefaultTableModel model = new DefaultTableModel();
-
-	        // Agregar columnas a la tabla
-	        model.addColumn("Nº REF");
-	        model.addColumn("FECHA");
-	        model.addColumn("PRECIO");
-	        model.addColumn("DESCRIPCIÓN");
-	        model.addColumn("ESTADO");
-	        model.addColumn("FECHA PREVISTA");
-
-
-
-	        // Agregar filas de datos a la tabla
-	        model.addRow(new Object[]{"", "", ""});
-	        model.addRow(new Object[]{"", "", ""});
-	        model.addRow(new Object[]{"", "", ""});
-	        model.addRow(new Object[]{"", "", ""});
+        pOeste.add(btnatras);
+        
+        //pCentroCen.add(txtEnv);
+        pCentroDer.add(txtRef);
+        pCentroDer.add(nRef);
+        pCentroIzq.add(btneliminar);
+        pCentro.add(pCentroDer, BorderLayout.NORTH);
+        pCentro.add(pCentroIzq, BorderLayout.CENTER);
+        pCentro.add(pCentroCen, BorderLayout.SOUTH);
         
 
+        add(pNorte, BorderLayout.NORTH);
+        add(pSur, BorderLayout.SOUTH);
+        add(pCentro, BorderLayout.CENTER);
+        add(pEste, BorderLayout.EAST);
+        add(pOeste, BorderLayout.WEST);
 
-	        // Crear la tabla con el modelo de datos
+        // Crear la tabla con el modelo de datos
+        DefaultTableModel model = new DefaultTableModel();
+        
+        model.addColumn("Nº Referencia");
+        model.addColumn("Fecha");
+        model.addColumn("Precio");
+        model.addColumn("Descripción");
+        model.addColumn("Estado");
+        model.addColumn("Fecha Prevista");
 
-	        JTable table = new JTable(model);
+        
+        model.addRow(new Object[]{"", "", ""});
+        model.addRow(new Object[]{"", "", ""});
+        model.addRow(new Object[]{"", "", ""});
+        model.addRow(new Object[]{"", "", ""});
+        model.addRow(new Object[]{"", "", ""});
+        model.addRow(new Object[]{"", "", ""});
+        model.addRow(new Object[]{"", "", ""});
 
-	        JPanel buttonPanel = new JPanel();
-	        JButton volverButton = new JButton("<--");
-	        JButton eliminarButton = new JButton("Eliminar Envío");
-	        JLabel txtenvio = new JLabel("Gestión de Envíos");
+        
+        table = new JTable(model);
 
-	        buttonPanel.add(volverButton);
-	        buttonPanel.add(eliminarButton);	       
+        // Ajustar el alto de las filas
+        int rowHeight = 30;  
+        table.setRowHeight(rowHeight);
+        
+        // Establecer márgenes
+        JPanel tablePanel = new JPanel(new BorderLayout());
+        tablePanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15)); 
+        
+        JScrollPane scrollPane = new JScrollPane(table);
+        tablePanel.add(scrollPane, BorderLayout.CENTER);
 
-	        // Establecer el diseño del marco como BorderLayout
-	        frame.setLayout(new BorderLayout());
+        pSur.add(tablePanel, BorderLayout.CENTER);
+        
+        // Agregar una etiqueta encima de la tabla
+        JLabel labelAboveTable = new JLabel("     Envíos realizados");
+        pSur.add(labelAboveTable, BorderLayout.NORTH); 
 
-	        // Agregar la tabla al centro del marco
-	        frame.add(new JScrollPane(table), BorderLayout.CENTER);
+        setTitle("Gestión");
+        setBounds(300, 18, 600, 400); // Ajustar el ancho del marco
+        setMinimumSize(new Dimension(700, 650)); // Configurar el tamaño mínimo de la ventana
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
 
-	        // Agregar el panel de botones en la esquina superior izquierda
-	        frame.add(buttonPanel, BorderLayout.NORTH);
-
-	        frame.add(txtenvio, BorderLayout.SOUTH);
-	        // Hacer visible el marco
-	        frame.setVisible(true);
-	    }
-	}
-
-
-
-
-
-	
-
+    public static void main(String[] args) {
+            VentanaGestionEmpleados ventana = new VentanaGestionEmpleados();
+        };
+    
+}
