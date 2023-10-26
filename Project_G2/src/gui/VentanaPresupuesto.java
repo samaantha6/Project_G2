0b21f179d6102573e949b0cf8f7bab06b55ba6e5
i.ruizdealegria <i.ruizdealegria@.opendeusto.es>
2023-10-26 14:01:57 +0200

    Comienzo VentanaPresupuesto

--- Project_G2/src/gui/VentanaPresupuesto.java
+++ Project_G2/src/gui/VentanaPresupuesto.java
@@ -1,10 +1,29 @@
 package gui;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class VentanaPresupuesto extends JFrame{
+	
+	private JPanel pNorte;
+	private JLabel txtPresupuesto, txtTipoEnvio;
+	private JCheckBox estandar, superior, premium;
+	private JButton btnHacerEnvio, btnVolver;
+	
 	public VentanaPresupuesto() {
 		
+		pNorte = new JPanel();
+		
+		txtPresupuesto = new JLabel("Presupuesto");
+		txtTipoEnvio = new JLabel("Tipo Envio");
+
+		estandar =new JCheckBox("Estandar\n(En 8/12 dias)");
+		superior =new JCheckBox("Superior\n(En 6/10 dias");
+		premium =new JCheckBox("Premium\\n(En 2 dias");
+
 		
 		
 	setTitle("Presupuesto");
