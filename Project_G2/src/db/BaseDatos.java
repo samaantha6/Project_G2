8e50c7a09633d7804859494a26a978d26949897b
sofia fernandez <so.fernandez@opendeusto.es>
2023-11-14 17:42:16 +0100

    prueba BD

--- Project_G2/src/db/BaseDatos.java
+++ Project_G2/src/db/BaseDatos.java
@@ -1,5 +1,22 @@
 package db;
 
-public class BaseDatos {
+import java.sql.Connection;
 
+public class BaseDatos {
+	
+	
+	/* Metodo que realiza la conexion con la base de datos*/
+	public static Connection initBD(String nombreBD) {
+		
+		Connection con = null;
+		
+		return con;
+		
+		
+	}
+
+	private void initBd() {
+		// TODO Auto-generated method stub
+
+	}
 }
