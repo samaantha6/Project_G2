1b58b52bae05ced2839df28d9b9f3c69b1b0c33e
josusaez <josumirena.saez@opendeusto.es>
2023-11-19 21:28:04 +0100

    CONFIGURACION de la BD

--- Project_G2/src/db/BaseDatosConfig.java
+++ Project_G2/src/db/BaseDatosConfig.java
@@ -1,5 +1,28 @@
 package db;
 
-public class BaseDatosConfig {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class BaseDatosConfig {
+	private static final String JDBC_URL = "jdbc:sqlite:./data/hermes.db";
+	
+	public static void createTable() {
+		try (Connection connection = DriverManager.getConnection(JDBC_URL);
+				Statement statement = connection.createStatement()) {
+			
+			String createTableSQL = "CREATE TABLE IF NOT EXISTS paquetes ("+
+									"id INT PRIMARY KEY, " +
+									"id_envio TEXT, " +
+									"remitente TEXT, " + 
+									"contenido TEXT, " +
+									"estado_entrega TEXT)";
+			statement.execute(createTableSQL);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
+	
+	
