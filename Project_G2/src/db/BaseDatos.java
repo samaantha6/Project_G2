e009929cc1dff97f2ce6d8a4df29582cb7e43185
josusaez <josumirena.saez@opendeusto.es>
2023-11-19 17:00:13 +0100

    IMPLEMENTACION ( PRUEBA ) BD

--- Project_G2/src/db/BaseDatos.java
+++ Project_G2/src/db/BaseDatos.java
@@ -1,22 +1,24 @@
 package db;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class BaseDatos {
+	private static final String JDBC_URL = "jdbc:sqlite:./data/sample.db";
 	
-	
-	/* Metodo que realiza la conexion con la base de datos*/
-	public static Connection initBD(String nombreBD) {
-		
-		Connection con = null;
-		
-		return con;
-		
-		
-	}
-
-	private void initBd() {
-		// TODO Auto-generated method stub
-
+	public static void createTable() {
+		try (Connection connection = DriverManager.getConnection(JDBC_URL);
+				Statement statement = connection.createStatement()) {
+			
+			String createTableSQL = "CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY, nombre TEXT)";
+			statement.execute(createTableSQL);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
+	
+	
+
